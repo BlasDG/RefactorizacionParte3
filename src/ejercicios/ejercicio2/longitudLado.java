0e2fbdbc1d2d65e560c5a2e3c8aae067f53a6c4b
BlasDG <blasdg2007@gmail.com>
2026-05-21 22:25:47 +0200

    Refactorizacion 4

--- src/ejercicios/ejercicio2/longitudLado.java
+++ src/ejercicios/ejercicio2/longitudLado.java
@@ -2,6 +2,7 @@ package ejercicios.ejercicio2;
 
 public class longitudLado {
     public static final int NUM_LADOS = 3;
+    public static final int DIVISOR_AREA = 4;
     private final int lado;
 
     public longitudLado(int lado) {
@@ -15,7 +16,7 @@ public class longitudLado {
         return lado * NUM_LADOS;
     }
     public double getArea() {
-        return (lado * lado * Math.sqrt(3)) / 4;
+        return (lado * lado * Math.sqrt(3)) / DIVISOR_AREA;
     }
 
 }
