b1999e7396a1cf62d3d10bcd74a538f48bd37903
BlasDG <blasdg2007@gmail.com>
2026-05-22 13:05:13 +0200

    Refactorizacion 9

--- src/ejercicios/ejercicio2/TrianguloEquilatero.java
+++ src/ejercicios/ejercicio2/TrianguloEquilatero.java
@@ -4,6 +4,7 @@ public class TrianguloEquilatero {
     public static final int NUM_LADOS = 3;
     public static final int DIVISOR_AREA = 4;
     private final int longitudLado;
+    private int lado;
 
     public TrianguloEquilatero(int longitudLado) {
         this.longitudLado = longitudLado;
@@ -23,5 +24,13 @@ public class TrianguloEquilatero {
 
     private static double calcularAreaInterna(int ladoCuadrado, double raizTres) {
         return (ladoCuadrado * raizTres) / DIVISOR_AREA;
+    }
+
+    public int getLado() {
+        return lado;
+    }
+
+    public void setLado(int lado) {
+        this.lado = lado;
     }
 }
